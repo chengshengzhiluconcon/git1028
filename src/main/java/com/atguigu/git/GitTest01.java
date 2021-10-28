6f0d2adf56380190cd45621b82548ac3ca4e1d8c
shujuzhijialeng <3358941163@qq.com>
2021-10-28 14:15:02 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+        System.out.println("git test3");
+
+    }
+}
